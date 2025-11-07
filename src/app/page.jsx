ee2560493ae1f15389a2e1670f443bfb3277fcad
Pablo Guzmán <pablocorporativa@gmail.com>
2025-11-07 12:39:22 -0300

    Fix: import Home.jsx with capital H

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 // src/app/page.jsx
-import Portfolio from '@/components/portfolio/home';
+import Portfolio from '@/components/portfolio/Home';
 
 export default function Home() {
   return <Portfolio />;
